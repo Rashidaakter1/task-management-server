739d7cf2e2cd9b171b249f9794cd2cb733e1a47d
Rashidaakter1 <rashidaakterchadni@gmail.com>
2022-07-01 00:48:25 +0600

    indexjs

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yxfsf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      console.log('mindo')
+    }
+    finally{
+
+    }
+}
+    run().catch(console.dir);
+
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log('Inside mongo')
+  client.close();
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
