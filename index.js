46309fe42f346cc7694202abd8a59ef644696180
Rashidaakter1 <rashidaakterchadni@gmail.com>
2022-07-01 10:30:01 +0600

    post

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 5000;
 
+
+const app = express()
+const port = process.env.PORT ||5000;
+
+//middle wear use
 app.use(cors())
 app.use(express.json())
 
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yxfsf.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uwms3.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run() {
-    try {
-      await client.connect();
-      console.log('mindo')
+async function run(){
+    try{
+        await client.connect()
+        const userToDoCollection = client.db("taskManagement").collection("task");
+        //post api
+        
+        app.get('/tasks',async(req,res)=>{
+            const query={}
+            const cursor = userToDoCollection.find(query)
+            const result = await cursor.toArray()
+
+          res.send(result)
+        })
+
+        app.post('/task', async (req,res)=>{
+            const data = req.body;
+            console.log(data)
+            const result = await userToDoCollection.insertOne(data);
+            res.send(result)
+        })
+
+        console.log('conect mongodb')
     }
     finally{
 
     }
 }
-    run().catch(console.dir);
-
-
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log('Inside mongo')
-  client.close();
-});
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!  ggg')
 })
 
 app.listen(port, () => {
